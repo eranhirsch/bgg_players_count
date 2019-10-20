814f8ec5efc1ea2fe4554b20c60416cb6c2c3e67
Eran Hirsch <hirsch@fb.com>
2019-10-20 22:02:56 +0300

    add subtypes

--- bgg/model/PlayItem.py
+++ bgg/model/PlayItem.py
@@ -1,4 +1,5 @@
 import xml.etree.ElementTree as ET
+from typing import List
 
 from utils import nullthrows
 
@@ -7,6 +8,7 @@ class PlayItem:
     __name: str
     __objectType: str
     __objectID: int
+    __subTypes: List[str]
 
     @staticmethod
     def fromElementTree(root: ET.Element) -> "PlayItem":
@@ -17,6 +19,10 @@ class PlayItem:
         item.__name = nullthrows(root.get("name"))
         item.__objectType = nullthrows(root.get("objecttype"))
         item.__objectID = int(nullthrows(root.get("objectid")))
+        item.__subTypes = [
+            nullthrows(subtype.get("value"))
+            for subtype in list(nullthrows(root.find("subtypes")))
+        ]
         return item
 
     def name(self) -> str:
@@ -27,3 +33,9 @@ class PlayItem:
 
     def objectID(self) -> int:
         return self.__objectID
+
+    def subTypes(self) -> List[str]:
+        return self.__subTypes
+
+    def __str__(self) -> str:
+        return f"PlayItem/Name: {self.name()}, Type: {self.objectType()}, ID: {self.objectID()}, Subtypes: {':'.join(self.subTypes())}"
