8bc604f853ad8e90a2ded7d1cb3e24d73e88fc64
Eran Hirsch <hirsch@fb.com>
2019-10-30 21:21:21 +0200

    parse all fields

--- bgg/model/GeekListItem.py
+++ bgg/model/GeekListItem.py
@@ -1,6 +1,44 @@
+import datetime
+
+from ..utils import nonthrows
 from .ModelBase import ModelBase
+
+DATE_FORMAT = "%a, %d %b %Y %H:%M:%S +0000"
 
 
 class GeekListItem(ModelBase):
+    def id(self) -> int:
+        return int(self._field("id"))
+
+    def object_type(self) -> str:
+        return self._field("objecttype")
+
+    def sub_type(self) -> str:
+        return self._field("subtype")
+
+    def object_id(self) -> int:
+        return int(self._field("objectid"))
+
+    def object_name(self) -> str:
+        return self._field("objectname")
+
+    def user_name(self) -> str:
+        return self._field("username")
+
+    def post_date(self) -> datetime.date:
+        return datetime.datetime.strptime(self._field("postdate"), DATE_FORMAT)
+
+    def edit_date(self) -> datetime.date:
+        return datetime.datetime.strptime(self._field("editdate"), DATE_FORMAT)
+
+    def thumbs(self) -> int:
+        return int(self._field("thumbs"))
+
+    def image_id(self) -> int:
+        return int(self._field("imageid"))
+
+    def body(self) -> str:
+        return nonthrows(nonthrows(self._root.find("body")).text)
+
     def _rootTagName(self) -> str:
         return "item"
--- list.py
+++ list.py
@@ -13,7 +13,9 @@ def main(argv=[]) -> int:
         return 1
 
     for item in items:
-        print(f"{item}")
+        print(
+            f"{item.edit_date()}, {item.id()}, {item.image_id()}, {item.object_id()}, {item.object_name()}, {item.object_type()}, {item.post_date()}, {item.sub_type()}, {item.thumbs()}, {item.user_name()}"
+        )
 
     return 0
 
