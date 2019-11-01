a3d07050f8e9dd92cb595e80d4f10a54191adb32
Eran Hirsch <hirsch@fb.com>
2019-11-01 20:13:24 +0200

    and use requestsbase for geeklists too

--- bgg/api/RequestList.py
+++ bgg/api/RequestList.py
@@ -1,21 +1,11 @@
-import time
 import xml.etree.ElementTree as ET
 from typing import Dict
 
-import requests
-
 from ..model.GeekList import GeekList
-from ..utils import nonthrows
-
-BASE_URL = "https://www.boardgamegeek.com/xmlapi/"
-
-HTTP_STATUS_CODE_OK = 200
-HTTP_STATUS_CODE_TOO_MANY_REQUESTS = 429
-
-MAX_RETRIES = 5
+from .RequestBase import RequestBase
 
 
-class RequestList:
+class RequestList(RequestBase[GeekList]):
     """
     A request for a geek list of items
     Defined in: https://boardgamegeek.com/wiki/page/BGG_XML_API#toc7
@@ -25,33 +15,24 @@ class RequestList:
         self.__listID = listid
 
     def fetch(self, with_comments: bool = False) -> GeekList:
-        uri = f"{BASE_URL}geeklist/{self.__listID}"
-        params: Dict[str, str] = {}
-        if with_comments:
+        return self._fetch(listid=self.__listID, with_comments=with_comments)
+
+    def _api_version(self) -> int:
+        return 1
+
+    def _api_path(self, **kwargs) -> str:
+        return f"geeklist/{kwargs['listid']}"
+
+    def _api_params(self, **kwargs) -> Dict[str, str]:
+        params = {}
+
+        if kwargs["with_comments"]:
             params["comments"] = "1"
 
-        for retries in range(MAX_RETRIES):
-            response = requests.get(uri, params=params)
-            root = ET.fromstring(response.text)
+        return params
 
-            if response.status_code == HTTP_STATUS_CODE_OK:
-                print(f"Geeklist found!")
-                results = GeekList(root)
-                return results
+    def _build_response(self, root: ET.Element) -> GeekList:
+        return GeekList(root)
 
-            elif response.status_code == HTTP_STATUS_CODE_TOO_MANY_REQUESTS:
-                if root.tag != "error":
-                    raise Exception(
-                        f"Unexpected error format, was exepecting 'error' tag but got {root.tag}"
-                    )
-                message = nonthrows(root.find("message")).text
-                retry_secs = 0.75 ** (retries)  # Exponential backoff
-                print(f'TOO MANY REQUESTS["{message}"]. Retrying in {retry_secs}s')
-                time.sleep(retry_secs)
-
-            else:
-                raise Exception(f"Bad API response: {response.status_code}")
-
-        raise Exception(
-            f"Bailing out! failed to fetch geeklist {self.__listID} after {retries} retries"
-        )
+    def _cache_file_name(str, **kwargs) -> str:
+        return kwargs["listid"]
