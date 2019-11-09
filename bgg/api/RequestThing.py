4f21fdeab108877e75e923681c14e14592b987a4
Eran Hirsch <hirsch@fb.com>
2019-11-09 20:18:37 +0200

    merge ThingItems into ThingItem

--- bgg/api/RequestThing.py
+++ bgg/api/RequestThing.py
@@ -1,7 +1,7 @@
 import xml.etree.ElementTree as ET
 from typing import Dict, Optional, Sequence, Set
 
-from ..model.ThingItems import ThingItems
+from ..model.ThingItem import ThingItems
 from ..utils import firstx
 from .RequestBase import RequestBase
 
--- bgg/model/ThingItem.py
+++ bgg/model/ThingItem.py
@@ -244,3 +244,25 @@ class ThingItem(ModelBase):
         """Raw data, prefer calling links instead"""
         for link in nonthrows(self._root.findall("link")):
             yield Link(link)
+
+
+class ThingItems(ModelBase, Sized, Iterable[ThingItem]):
+    @classmethod
+    def _rootTagName(cls) -> str:
+        return "items"
+
+    def with_flags(self, flags: Set[str]) -> "ThingItems":
+        self.__flags = flags
+        return self
+
+    def only_item(self) -> ThingItem:
+        if len(self) > 1:
+            raise Exception(f"There is more than one item in the response {len(self)}")
+        return firstx(self)
+
+    def __len__(self) -> int:
+        return len(self._root)
+
+    def __iter__(self) -> Iterator[ThingItem]:
+        for item in self._root:
+            yield ThingItem(item).with_flags(self.__flags)
