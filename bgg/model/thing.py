e6c360d5713577e87cb5be7fdceac8e4e644f73f
Eran Hirsch <hirsch@fb.com>
2019-11-25 19:14:43 +0200

    update error message for flags

--- bgg/model/thing.py
+++ bgg/model/thing.py
@@ -286,7 +286,7 @@ class Item(ModelBase):
     def __assert_flag(self, flag: str) -> None:
         if flag not in self.__flags:
             raise Exception(
-                f"{flag} data not requested! Add 'with_{flag}' to the query"
+                f"{flag} data not requested! Add '{flag}' to query flags"
             )
 
     def __assert_type(self, type: str) -> None:
