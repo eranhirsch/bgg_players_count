9ebd38be5a326f72659fe15e4a27054682d1eb49
Eran Hirsch <hirsch@fb.com>
2019-11-25 12:26:17 +0200

    extract method

--- bar_chart_race.py
+++ bar_chart_race.py
@@ -14,6 +14,7 @@ from observers import BarChartRace as bcr
 SEPARATOR = "\t"
 MISSING_CATEGORY_LABEL = "[Unknown]"
 COLLECTED_FAMILIES: Set[int] = {36963, 39442, 54682}
+MIN_PLAYS_FOR_DISPLAY = 50
 
 g_games_in_family: Dict[str, Set[int]] = defaultdict(set)
 
@@ -74,10 +75,17 @@ def process_games(aggr_by: int, games: Iterable[int]) -> Iterator[str]:
         )
 
         bar_chart_race = bcr.Logic(aggr_by)
+        plays = 0
         for play in RequestPlays(thingid=game_id).queryAll():
             bar_chart_race.visit(play)
+            plays += 1
 
-        yield f"{SEPARATOR.join(metadata)}{SEPARATOR}{bcr.Presenter(bar_chart_race, window(aggr_by), SEPARATOR)}\n"
+        if plays >= MIN_PLAYS_FOR_DISPLAY:
+            yield f"{SEPARATOR.join(metadata)}{SEPARATOR}{bcr.Presenter(bar_chart_race, window(aggr_by), SEPARATOR)}\n"
+        else:
+            print(
+                f"Game {game.primary_name()} ({game.id()}) only had {plays} plays so it won't be added to the output!"
+            )
 
         index += 1
 
@@ -89,50 +97,54 @@ def process_families(aggr_by: int) -> Iterator[str]:
         if not family_games:
             raise Exception(f"Family {family_name} has no entries in it!")
 
-        name = (
-            unicodedata.normalize("NFKD", family_name)
-            .encode("ascii", "ignore")
-            .decode("ascii")
-        )
-
-        bar_chart_race = bcr.Logic(aggr_by)
-
-        earliest_year = None
-        popular_category = None
-        popular_thumbnail = None
-        popular_users_rated = 0
-        index = 1
-        for game_id in family_games:
-            game = RequestThing(game_id).query(with_stats=True).only_item()
-
-            print(
-                f"Adding {index:03d} {game.primary_name()} ({game.id()}) to family {name}"
-            )
-
-            if game.ratings().users_rated() > popular_users_rated:
-                popular_users_rated = game.ratings().users_rated()
-                popular_category = game.primary_category()
-                popular_thumbnail = game.thumbnail()
-
-            if not earliest_year or game.year_published() < earliest_year:
-                earliest_year = game.year_published()
-
-            for play in RequestPlays(thingid=game_id).queryAll():
-                bar_chart_race.visit(play)
-
-            index += 1
-
-        metadata = [
-            f"{earliest_year}-{name}",
-            popular_category or MISSING_CATEGORY_LABEL,
-            popular_thumbnail or "",
-        ]
-
-        yield f"{SEPARATOR.join(metadata)}{SEPARATOR}{bcr.Presenter(bar_chart_race, window(aggr_by), SEPARATOR)}\n"
-
-        print(f"Finished processing family {name}, it has {index-1} games in it")
+        yield process_family(aggr_by, family_name, family_games)
 
     print(f"Finished processing all families")
+
+
+def process_family(aggr_by: int, family_name: str, family_games: Iterable[int]) -> str:
+    name = (
+        unicodedata.normalize("NFKD", family_name)
+        .encode("ascii", "ignore")
+        .decode("ascii")
+    )
+
+    bar_chart_race = bcr.Logic(aggr_by)
+
+    earliest_year = None
+    popular_category = None
+    popular_thumbnail = None
+    popular_users_rated = 0
+    index = 1
+    for game_id in family_games:
+        game = RequestThing(game_id).query(with_stats=True).only_item()
+
+        print(
+            f"Adding {index:03d} {game.primary_name()} ({game.id()}) to family {name}"
+        )
+
+        if game.ratings().users_rated() > popular_users_rated:
+            popular_users_rated = game.ratings().users_rated()
+            popular_category = game.primary_category()
+            popular_thumbnail = game.thumbnail()
+
+        if not earliest_year or game.year_published() < earliest_year:
+            earliest_year = game.year_published()
+
+        for play in RequestPlays(thingid=game_id).queryAll():
+            bar_chart_race.visit(play)
+
+        index += 1
+
+    metadata = [
+        f"{earliest_year}-{name}",
+        popular_category or MISSING_CATEGORY_LABEL,
+        popular_thumbnail or "",
+    ]
+
+    print(f"Finished processing family {name}, it has {index-1} games in it")
+
+    return f"{SEPARATOR.join(metadata)}{SEPARATOR}{bcr.Presenter(bar_chart_race, window(aggr_by), SEPARATOR)}\n"
 
 
 if __name__ == "__main__":
